1ddaf98f6ab7f73a122f14a689c83701d6fee85d
tutn <azjustin3@gmail.com>
2024-05-28 21:41:38 +0700

    Update model

--- src/common/models/quiz.model.ts
+++ src/common/models/quiz.model.ts
@@ -5,6 +5,7 @@ import { Writing } from './writing.model';
 export interface Quiz {
   id: string;
   name: string;
+  audioName?: string;
   audioUrl?: string;
   listeningTimeout?: number;
   readingTimeout?: number;
