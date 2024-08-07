8055733e6531ad48da512d060bf9fa507965cdce
tutn <azjustin3@gmail.com>
2024-08-08 03:35:57 +0700

    Update server

--- server/server.js
+++ server/server.js
@@ -11,15 +11,17 @@ server.use("/upload", express.static(`${__dirname}/upload`));
 
 // Routers
 const router = jsonServer.router("db.json");
+const systemRouter = require('./routes/system.router')
 const emailRouter = require("./routes/mail.router");
 const fileRouter = require("./routes/file.router");
 const folderRouter = require("./routes/folder.router");
 const quizRouter = require("./routes/quiz.router");
 
+server.use("/api/system", systemRouter);
 server.use("/api/file", fileRouter);
 server.use("/api/mail", emailRouter);
 server.use("/api/folders", folderRouter);
-server.use("/api/quizzes", quizRouter);
+server.use("/api/quizz", quizRouter);
 server.use("/api", router);
 server.get("*", function (req, res) {
   res.sendFile(__dirname + "/dist/browser/index.html");
