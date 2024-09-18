218c5e5f4eb81d366ec01dcb4fe967f08e2666ee
tutn <azjustin3@gmail.com>
2024-09-19 01:58:08 +0700

    Update server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const session = require("express-session");
-const jsonServer = require("json-server");
 const cors = require("cors");
 const morgan = require("morgan");
 const server = express();
-const fs = require("fs");
 const dbConfig = require("./configs/db.config");
 const passport = require("passport");
 const passportConfig = require("./configs/passport.config");
@@ -14,6 +13,7 @@ server.use(cors());
 server.use(morgan("tiny"));
 server.use(express.json());
 server.use("/upload", express.static(`${__dirname}/upload`));
+server.use(express.static(path.join(__dirname, 'dist/browser')));
 server.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -22,36 +22,22 @@ server.use(
   }),
 );
 server.use(passport.initialize());
-server.use(passport.session());
 passportConfig(passport);
 
 // Routers
-const router = jsonServer.router("db.json");
 const authRouter = require("./routes/auth.router");
-const systemRouter = require("./routes/system.router");
 const emailRouter = require("./routes/mail.router");
 const fileRouter = require("./routes/file.router");
-const quizRouter = require("./routes/quiz.router");
 const questionRouter = require("./routes/question.router");
 const choiceRouter = require("./routes/choice.router");
 const selfReadingRouter = require("./routes/self-reading.router");
 
 server.use("/api/auth", authRouter);
-server.use("/api/system", systemRouter);
 server.use("/api/file", fileRouter);
 server.use("/api/mail", emailRouter);
-server.use("/api/quizzes", quizRouter);
-server.use("/api/system", systemRouter);
 server.use("/api/self-reading", selfReadingRouter);
 server.use("/api/questions", questionRouter);
 server.use("/api/choices", choiceRouter);
-server.use("/api", router);
-const dbPath = "db.json";
-fs.watchFile(dbPath, (curr, prev) => {
-  console.log("db.json file changed. Reloading database...");
-  router.db.read();
-});
-
 server.get("*", function (req, res) {
   res.sendFile(__dirname + "/dist/browser/index.html");
 });
