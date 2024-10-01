37c2c087eaa0fa185c269404ad1da9466a38c8eb
tutn <azjustin3@gmail.com>
2024-10-02 05:16:37 +0700

    update logic for multiple choices

---
+++ src/app/modules/question/multiple-choices/multiple-choices-testing/multiple-choices-testing.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { AbstractQuestionComponent } from '../../../../shared/abstract/abstract-question.component';
+import { MatIcon } from '@angular/material/icon';
+import { AngularEditorModule } from '@wfpena/angular-wysiwyg';
+import { FormsModule } from '@angular/forms';
+import { MultipleChoicesEditingComponent } from '../multiple-choices-editing/multiple-choices-editing.component';
+import { filter, isEmpty, isNull } from 'lodash-es';
+
+@Component({
+  selector: 'app-multiple-choices-testing',
+  standalone: true,
+  imports: [MatIcon, AngularEditorModule, FormsModule],
+  templateUrl: './multiple-choices-testing.component.html',
+  styleUrl: './multiple-choices-testing.component.scss',
+})
+export class MultipleChoicesTestingComponent extends AbstractQuestionComponent {
+  selectedOption: string | null = '';
+  mapSelectedChoice: Record<number, boolean> = {};
+
+  override ngOnInit() {
+    super.ngOnInit();
+    if (
+      !isEmpty(this.question.answer) &&
+      Number(this.question.numberOfChoices) < 2
+    ) {
+      this.selectedOption = this.question.answer[0];
+    }
+  }
+
+  onSelectSingleAnswer(option: string) {
+    this.selectedOption = this.selectedOption === option ? null : option;
+    if (isNull(this.selectedOption)) {
+      this.question.answer = [];
+    } else {
+      this.question.answer = [option];
+    }
+  }
+
+  onSelectMultipleAnswer(event: MouseEvent, choiceId: string) {
+    if (
+      !this.question.answer.includes(choiceId) &&
+      this.isReachedMaxAnswers(this.question.answer.length)
+    ) {
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+    if (this.question.answer.includes(choiceId)) {
+      this.question.answer = filter(
+        this.question.answer,
+        (ans) => ans !== choiceId,
+      );
+    } else {
+      (this.question.answer as string[]).push(choiceId);
+    }
+  }
+
+  private isReachedMaxAnswers(numberOfSelectdChoice: number) {
+    return Number(this.question.numberOfChoices) === numberOfSelectdChoice;
+  }
+}
--- src/app/modules/question/multiple-choices/multiple-choices.component.ts
+++ src/app/modules/question/multiple-choices/multiple-choices.component.ts
@@ -13,6 +13,9 @@ import { filter, isArray, isEmpty, isNull } from 'lodash-es';
 import { CHOICE_INDEX } from '../../../utils/constant';
 import { CorrectChoicesPipe } from '../../../pipes/correct-choices.pipe';
 import { MatRadioGroup } from '@angular/material/radio';
+import { MultipleChoicesEditingComponent } from "./multiple-choices-editing/multiple-choices-editing.component";
+import { MultipleChoicesTestingComponent } from "./multiple-choices-testing/multiple-choices-testing.component";
+import { MultipleChoicesReadonlyComponent } from "./multiple-choices-readonly/multiple-choices-readonly.component";
 
 @Component({
   selector: 'app-multiple-choices',
@@ -28,120 +31,13 @@ import { MatRadioGroup } from '@angular/material/radio';
     MatCardModule,
     CorrectChoicesPipe,
     MatRadioGroup,
+    MultipleChoicesEditingComponent,
+    MultipleChoicesTestingComponent,
+    MultipleChoicesReadonlyComponent,
   ],
   providers: [FileService],
   templateUrl: './multiple-choices.component.html',
   styleUrl: './multiple-choices.component.scss',
 })
 export class MultipleChoicesComponent extends AbstractQuestionComponent {
-  selectedOption: string | null = '';
-  mapSelectedChoice: Record<number, boolean> = {};
-  CHOICE_INDEX: string[] = CHOICE_INDEX;
-
-  override ngOnInit() {
-    super.ngOnInit();
-    if (
-      this.isTesting &&
-      !isEmpty(this.question.answer) &&
-      Number(this.question.numberOfChoices) < 2
-    ) {
-      this.selectedOption = this.question.answer[0];
-    }
-    if (
-      !this.isTesting &&
-      !isEmpty(this.question.correctAnswer) &&
-      Number(this.question.numberOfChoices) < 2
-    ) {
-      this.selectedOption = this.question.correctAnswer[0];
-    }
-  }
-
-  onSelectSingleCorrectAnswer(option: string) {
-    this.selectedOption = this.selectedOption === option ? null : option;
-    if (isNull(this.selectedOption)) {
-      this.question.correctAnswer = [];
-    } else {
-      this.question.correctAnswer = [option];
-    }
-  }
-
-  onSelectSingleAnswer(option: string) {
-    this.selectedOption = this.selectedOption === option ? null : option;
-    if (isNull(this.selectedOption)) {
-      this.question.answer = [];
-    } else {
-      this.question.answer = [option];
-    }
-  }
-
-  onSelectMultipleCorrectAnswer(event: MouseEvent, choiceId: string) {
-    if (
-      !this.question.correctAnswer.includes(choiceId) &&
-      this.isReachedMaxAnswers(this.question.correctAnswer.length)
-    ) {
-      event.preventDefault();
-      event.stopPropagation();
-      return;
-    }
-    if (this.question.correctAnswer.includes(choiceId)) {
-      this.question.correctAnswer = filter(
-        this.question.correctAnswer,
-        (ans) => ans !== choiceId,
-      );
-    } else {
-      this.question.correctAnswer.push(choiceId);
-    }
-  }
-
-  private isReachedMaxAnswers(numberOfSelectdChoice: number) {
-    return Number(this.question.numberOfChoices) === numberOfSelectdChoice;
-  }
-
-  onSelectMultipleAnswer(event: MouseEvent, choiceId: string) {
-    if (
-      !this.question.answer.includes(choiceId) &&
-      this.isReachedMaxAnswers(this.question.answer.length)
-    ) {
-      event.preventDefault();
-      event.stopPropagation();
-      return;
-    }
-    if (this.question.answer.includes(choiceId)) {
-      this.question.answer = filter(
-        this.question.answer,
-        (ans) => ans !== choiceId,
-      );
-    } else {
-      (this.question.answer as string[]).push(choiceId);
-    }
-  }
-
-  onSelectChoice(index: number) {
-    if (this.isReadOnly) {
-      return;
-    }
-    this.mapSelectedChoice[index] = !this.mapSelectedChoice[index];
-    if (this.isEditing) {
-      const id = this.question.choices[index].id;
-      if (!this.question.correctAnswer?.includes(id)) {
-        this.question.correctAnswer.push(id);
-      } else {
-        this.question.correctAnswer = this.question.correctAnswer.filter(
-          (correctAnswerId) => correctAnswerId !== id,
-        );
-      }
-    }
-    if (this.isTesting) {
-      if (isArray(this.question.answer)) {
-        const id = this.question.choices[index].id;
-        if (!this.question.answer?.includes(id)) {
-          this.question.answer.push(id);
-        } else {
-          this.question.answer = this.question.answer.filter(
-            (answerId) => answerId !== id,
-          );
-        }
-      }
-    }
-  }
 }
